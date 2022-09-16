620d15eba7cb21d4335a07f43d10154c5924ef86
Martin Průša <73097610+Martin-Prusa@users.noreply.github.com>
2022-09-16 12:06:10 +0200

    i

--- src/main/java/cz/martin/spirala/Main.java
+++ src/main/java/cz/martin/spirala/Main.java
@@ -1,7 +1,17 @@
 package cz.martin.spirala;
 
 public class Main {
+    private int num = 4;
+    private int[][] field = new int[num][num];
     public static void main(String[] args) {
-        int num = 4;
+        // 1  2  3  4
+        // 12 13 14 5
+        // 11 16 15 6
+        // 10 9  8  7
+
+    }
+
+    public static void setNext(int x, int y, int dirX, int dirY, int num) {
+
     }
 }
